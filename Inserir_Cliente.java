a1d1c524e76af6a92ad8a40b79ce9aaa1aedae31
curso <carlinhosdeodoro@gmail.com>
2018-10-18 20:41:28 -0300

    aaa

--- Inserir_Cliente.java
+++ Inserir_Cliente.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,16 +14,16 @@ import android.database.Cursor;
 
 public class Inserir_Cliente extends AppCompatActivity {
 
-    String vTipo;
     Intent intent;
     Pessoa pessoa;
+    Button btnCadastrar;
+    Button btvoltar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inserir__cliente);
-        Button btvoltar = (Button)findViewById(R.id.btvoltar);
         btvoltar = (Button)findViewById(R.id.btvoltar);
-        Button btnCadastrar = (Button) findViewById(R.id.btnCadastrar);
+        btnCadastrar = (Button) findViewById(R.id.btnCadastrar);
         intent = getIntent();
 
         final EditText txtNome = (EditText) findViewById(R.id.txtNome);
@@ -30,7 +31,7 @@ public class Inserir_Cliente extends AppCompatActivity {
 
 
 
-        intent = getIntent();
+       /* intent = getIntent();
         if(intent != null) {
             pessoa = intent.getExtras().getParcelable("pessoa");
 
@@ -39,26 +40,26 @@ public class Inserir_Cliente extends AppCompatActivity {
                 txtCPF.setText(pessoa.getCpf());
             }
         }
-
+*/
         btnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
-                if(pessoa == null) {
-                    DbHelper dbhelper = new DbHelper(getBaseContext());
-                    Pessoa p = new Pessoa(null);
-                    p.setNome(txtNome.getText().toString());
-                    p.setCpf(txtCPF.getText().toString());
-                    dbhelper.SalvarPessoa(p);
-                }
-                else {
+try {
+    DbHelper dbhelper = new DbHelper(getBaseContext());
+    Pessoa p = new Pessoa(null);
+    p.setNome(txtNome.getText().toString());
+    p.setCpf(txtCPF.getText().toString());
+    dbhelper.SalvarPessoa(p);
+
+               /* else {
                     DbHelper dbHelper = new DbHelper(getBaseContext());
                     Intent intent = getIntent();
                     Integer id = intent.getIntExtra("ID",0);
-                    pessoa.setNome(txtNome.getText().toString());
-                    pessoa.setCpf(txtCPF.getText().toString());
-                    dbHelper.Alterar(pessoa, id);
+                    pessoa_db.setNome(txtNome.getText().toString());
+                    pessoa_db.setCpf(txtCPF.getText().toString());
+                    dbHelper.Alterar(pessoa_db, id);
 
-                }
+                }*/
 
 
                 /*nt idcliente = 0;
@@ -73,9 +74,11 @@ public class Inserir_Cliente extends AppCompatActivity {
                 contentValues.put("CPF", txtCPF.getText().toString());
                 long newRowId;
                 newRowId = db.insert("CLIENTES","CODIGO_CLIENTE" , contentValues);*/
-                Toast.makeText(getBaseContext(), "Cadastro realizado com sucesso", Toast.LENGTH_SHORT).show();
+    Toast.makeText(getBaseContext(), "Cadastro realizado com sucesso", Toast.LENGTH_SHORT).show();
 
-
+}catch ( Exception e){
+    Log.e("ERRO!", "ERRO");
+}
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
